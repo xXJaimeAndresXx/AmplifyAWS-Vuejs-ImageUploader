bf088bdddf9df83f34203d2c305d83419ec5cd96
Jaime Andres <jaimeandres2998@gmail.com>
2020-09-25 13:35:04 -0500

    Uploader

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+      <div v-if="!signedIn">
+         <amplify-authenticator></amplify-authenticator>
+      </div>
+      <div v-if="signedIn">
+        <amplify-sign-out class="signout"></amplify-sign-out>
+        <div class="container">
+          <amplify-photo-picker
+            v-bind:photoPickerConfig="photoPickerConfig"
+          ></amplify-photo-picker>
+          <amplify-s3-album path="images/"></amplify-s3-album>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { AmplifyEventBus } from 'aws-amplify-vue'
+import { Auth } from 'aws-amplify'
+const photoPickerConfig = {
+  path: 'images/',
+}
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  async beforeCreate() {
+    try {
+      const user = await Auth.currentAuthenticatedUser()
+      this.signedIn = true
+    } catch (err) {
+      this.signedIn = false
+    }
+    AmplifyEventBus.$on('authState', info => {
+      if (info === 'signedIn') {
+        this.signedIn = true
+      } else {
+        this.signedIn = false
+      }
+    });
+  },
+  data () {
+    return {
+      photoPickerConfig,
+      signedIn: false
+    }
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  padding: 40px;
+}
+.signout {
+  background-color: #ededed;
+  margin: 0;
+  padding: 11px 0px 1px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Amplify, * as AmplifyModules from 'aws-amplify';
+import { AmplifyPlugin } from 'aws-amplify-vue';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);Vue.use(AmplifyPlugin, AmplifyModules);
 
 Vue.config.productionTip = false
 
